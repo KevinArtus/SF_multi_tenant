6efcf13e163d59339208e82ffcc43890575308a6
Arno <arnaudlepeltier@yahoo.fr>
2019-04-06 23:48:58 +0200

    Add twig extension

---
+++ src/Twig/AgencyExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Twig;
+
+use App\ContextStorage;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AgencyExtension extends AbstractExtension
+{
+    /**
+     *
+     * @var ContextStorage
+     */
+    private $contextStorage;
+    
+    public function __construct(ContextStorage $contextStorage)
+    {
+        $this->contextStorage = $contextStorage;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('context_agency', [$this->contextStorage, 'getAgency']),
+        ];
+    }
+    
+
+}
